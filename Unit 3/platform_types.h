2b5f36ed2fe112c32eaac979528eafc55bfd369a
Mazen Omar <112568201+Mazen-Omar@users.noreply.github.com>
2024-06-29 18:43:54 +0300

    Update

--- Unit 3/platform_types.h
+++ Unit 3/platform_types.h
@@ -1,17 +1,14 @@
 #ifndef PLATFORM_TYPES_H
 #define PLATFORM_TYPES_H
 
-typedef signed char sint8;
-typedef unsigned char uint8;
+typedef signed char       sint8;
+typedef unsigned char     uint8;
 
-typedef signed short sint16;
-typedef unsigned short uint16;
+typedef signed short     sint16;
+typedef unsigned short   uint16;
 
-typedef signed int sint32;
-typedef unsigned int uint32;
-
-typedef signed long sint64;
-typedef unsigned long uint64;
+typedef signed int       sint32;
+typedef unsigned int     uint32;
 
 #endif
 
