039a8357f341377629a1da4699ac63ee7eb7b93e
Mazen Omar <mazenomar741@gmail.com>
2024-06-29 20:14:23 +0300

    Upload

---
+++ Unit 3/Assignment_1/platform_types.h
@@ -0,0 +1,15 @@
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+typedef signed char       sint8;
+typedef unsigned char     uint8;
+
+typedef signed short     sint16;
+typedef unsigned short   uint16;
+
+typedef signed int       sint32;
+typedef unsigned int     uint32;
+
+#endif
+
+
