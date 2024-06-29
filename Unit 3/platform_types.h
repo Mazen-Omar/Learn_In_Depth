e527e68bf409d557c1e65f86e2a037539bc0ba1e
Mazen Omar <mazenomar741@gmail.com>
2024-06-29 18:35:35 +0300

    Upload platform_types.h

---
+++ Unit 3/platform_types.h
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+
+typedef signed char sint8;
+typedef unsigned char uint8;
+
+typedef signed short sint16;
+typedef unsigned short uint16;
+
+typedef signed int sint32;
+typedef unsigned int uint32;
+
+typedef signed long sint64;
+typedef unsigned long uint64;
